ce0af79c309873e90ee7f7a52443b964270a3715
hundredball <chh008@eng.ucsd.edu>
2021-07-29 08:18:57 -0700

    Add L1 regularization

--- plot_confusion_matrix.py
+++ plot_confusion_matrix.py
@@ -34,21 +34,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.utils.multiclass import unique_labels
 
-## import some data to play with
-#iris = datasets.load_iris()
-#X = iris.data
-#y = iris.target
-#class_names = iris.target_names
-#
-## Split the data into a training set and a test set
-#X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-#
-## Run classifier, using a model that is too regularized (C too low) to see
-## the impact on the results
-#classifier = svm.SVC(kernel='linear', C=0.01)
-#y_pred = classifier.fit(X_train, y_train).predict(X_test)
-
-
 def plot_confusion_matrix(y_true, y_pred, classes, fileName=None,
                           normalize=False,
                           title=None,
@@ -108,16 +93,3 @@ def plot_confusion_matrix(y_true, y_pred, classes, fileName=None,
     else:
         plt.close()
     return ax
-
-
-#np.set_printoptions(precision=2)
-#
-## Plot non-normalized confusion matrix
-#plot_confusion_matrix(y_test, y_pred, classes=class_names,
-#                      title='Confusion matrix, without normalization')
-#
-## Plot normalized confusion matrix
-#plot_confusion_matrix(y_test, y_pred, classes=class_names, normalize=True,
-#                      title='Normalized confusion matrix')
-#
-#plt.show()
--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LogisticRegression
 
 import dataloader
+
+def select_features(X_train, X_test, Y_train):
+    
+    assert isinstance(X_train, np.ndarray) and X_train.ndim==2
+    
+    sel_ = SelectFromModel(LogisticRegression(C=1, penalty='l1', solver='liblinear'))
+    sel_.fit(X_train, Y_train)
+    indices = np.where(sel_.estimator_.coef_!=0)[0]
+    
+    return X_train[:, indices], X_test[:, indices]
+    
 
 def avg_channels_into_regions(EEG_list, df_all):
     '''
