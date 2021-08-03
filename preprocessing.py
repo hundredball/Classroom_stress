45f4b98b9c8acd7f42d72cafc342006fb9a64529
hundredball <chh008@eng.ucsd.edu>
2021-08-03 13:58:48 -0700

    Remove bad trials and run t-test

--- preprocessing.py
+++ preprocessing.py
@@ -5,13 +5,30 @@ from sklearn.linear_model import LogisticRegression
 
 import dataloader
 
+def remove_trials(EEG_list, labels, df_all, remove_indices = None):
+    '''
+    Remove trials of given indices
+    '''
+    
+    # Bad trials by visual inspection
+    if remove_indices == None:
+        remove_indices = {96, 97, 101, 109, 111, 113, 115, 116, 119, 141, 147, 164}
+    
+    select_indices = [i for i in range(len(EEG_list)) if i not in remove_indices]
+    
+    EEG_list = [EEG_list[i] for i in range(len(EEG_list)) if i in select_indices]
+    labels = labels[select_indices]
+    df_all = df_all.iloc[select_indices,:].reset_index(drop=True)
+    
+    return EEG_list, labels, df_all
+
 def select_features(X_train, X_test, Y_train):
     
     assert isinstance(X_train, np.ndarray) and X_train.ndim==2
     
     sel_ = SelectFromModel(LogisticRegression(C=1, penalty='l1', solver='liblinear'))
     sel_.fit(X_train, Y_train)
-    indices = np.where(sel_.estimator_.coef_!=0)[0]
+    indices = np.where(sel_.estimator_.coef_!=0)[1]
     
     return X_train[:, indices], X_test[:, indices]
     
@@ -64,5 +81,8 @@ def avg_channels_into_regions(EEG_list, df_all):
             EEG_regions[i_region,:] = np.mean(EEG_list[i][indices_channels[i_region],:], axis=0)
         
         EEG_regions_list.append(EEG_regions)
+        
+    # Replace channels with regions
+    df_all['channels'] = [['F','LT','C','RT','P','O'] for _ in range(len(df_all))]
         
     return EEG_regions_list
