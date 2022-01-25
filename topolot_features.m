ff76312e66d0448a3e02f565783fd9a9391ca13b
hundredball <chh008@eng.ucsd.edu>
2022-01-24 23:51:07 -0800

    Modified topoplot_features.m

--- topolot_features.m
+++ topolot_features.m
@@ -13,17 +13,28 @@ chanlocs = EEG.chanlocs;
 
 figure();
 figure_count = 1;
+
+% Search maximum
+maximum = 0;
+for band = {'theta', 'alpha', 'beta', 'gamma'}
+    counts_band = counts.(band{1});
+    
+    if max(counts_band) > maximum
+        maximum = max(counts_band);
+    end
+end
+
 % Plot topoplots for each band
 for band = {'theta', 'alpha', 'beta', 'gamma'}
     counts_band = counts.(band{1});
     
-    %figure('Name', band{1});
     subplot(2, 2, figure_count);
     topoplot( counts_band, chanlocs([4,11,14,15,22,31]) );
     title(band{1});
-    caxis([0,20]);
     
-    %saveas(gcf, [folder band{1} '.png']);
+    % Set colorbar scale
+    caxis([0, maximum]);
+    
     figure_count = figure_count + 1;
 end
 
@@ -31,10 +42,16 @@ end
 colorbar('Position', [0.9, 0.3, 0.01, 0.4]);
 
 % Build title
+if contains(feature, 'XGB')
+    title_type = 'feature_importances_';
+else
+    title_type = 'degree_of_use_';
+end
+
 axes( 'Position', [0, 0.95, 1, 0.05] ) ;
 set( gca, 'Color', 'None', 'XColor', 'White', 'YColor', 'White' ) ;
-text( 0.5, 0, 'Degree of use', 'FontSize', 14', 'FontWeight', 'Bold', ...
+text( 0.5, 0, title_type, 'FontSize', 14', 'FontWeight', 'Bold', ...
   'HorizontalAlignment', 'Center', 'VerticalAlignment', 'Bottom' ) ;
 
 % Save figure
-saveas(gcf, [folder 'degree_of_use_' feature '.png']);
+saveas(gcf, [folder 'feature_topoplot.png']);
