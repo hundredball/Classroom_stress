ac48a0cf649b4da01a1870094a4335740e18bf70
hundredball <chh008@eng.ucsd.edu>
2022-02-17 22:23:48 -0800

    Add sliding window to increase data samples

--- preprocessing.py
+++ preprocessing.py
@@ -10,10 +10,11 @@ import dataloader
 
 fs = 1000
 
+
 class StressData:
-    
+
     def __init__(self, EEG_list, labels, df_all):
-        '''
+        """
         EEG_list: list of 2D array
             i: trial
             j: channel
@@ -22,66 +23,67 @@ class StressData:
             labels regarding stress level
         df_all: DataFrame
             information for each trial (channels, stress, file name...)
-        
-        '''
+
+        """
         self.EEG_list = EEG_list
         self.labels = labels
         self.df_all = df_all
-        
+        self.sample_indices = list(range(len(EEG_list)))  # Used for referencing data in sliding window
+
         # Remove trials that have mismatch number of channels in EEG_list and df_all
         remove_indices = set()
         for i in range(len(self.EEG_list)):
-            if self.EEG_list[i].shape[0] != len(self.df_all.loc[i,'channels']):
+            if self.EEG_list[i].shape[0] != len(self.df_all.loc[i, 'channels']):
                 remove_indices.add(i)
-        if remove_indices:        
+        if remove_indices:
             print('Remove due to mismatch number of channels in EEG_list and df_all')
             self.remove_trials(remove_indices)
-        
+
     def remove_abnormal_trials(self, tol=1e4):
-        '''
+        """
         Remove trials with time signal larger than tol or smaller than -tol
-        '''
-        
+        """
+
         remove_indices = []
-        
+
         for i in range(len(self.EEG_list)):
-            if np.any(self.EEG_list[i]>=tol) or np.min(self.EEG_list[i]<=-tol):
+            if np.any(self.EEG_list[i] >= tol) or np.min(self.EEG_list[i] <= -tol):
                 remove_indices.append(i)
-                
-        
-        self.remove_trials(remove_indices = remove_indices)
-        
-    def remove_trials(self, remove_indices = None):
-        '''
+
+        self.remove_trials(remove_indices=remove_indices)
+
+    def remove_trials(self, remove_indices=None):
+        """
         Remove trials of given indices
-        '''
+        """
 
         # Bad trials by visual inspection
         if remove_indices == None:
             remove_indices = {96, 97, 101, 109, 111, 113, 115, 116, 119, 141, 147, 164}
         print('Remove trials:')
         for index in remove_indices:
-            print('Number: %d, Session: %s, Subject: %d, Label: %d'%(
-                self.df_all.loc[index,'number'], self.df_all.loc[index,'session'], 
-                self.df_all.loc[index,'subject'], self.labels[index]))
+            print('Number: %d, Session: %s, Subject: %d, Label: %d' % (
+                self.df_all.loc[index, 'number'], self.df_all.loc[index, 'session'],
+                self.df_all.loc[index, 'subject'], self.labels[index]))
 
         select_indices = [i for i in range(len(self.EEG_list)) if i not in remove_indices]
 
         self.EEG_list = [self.EEG_list[i] for i in range(len(self.EEG_list)) if i in select_indices]
-        self.labels = self.labels[select_indices] 
-        self.df_all = self.df_all.iloc[select_indices,:].reset_index(drop=True)
+        self.labels = self.labels[select_indices]
+        self.df_all = self.df_all.iloc[select_indices, :].reset_index(drop=True)
+        self.sample_indices = list(range(len(self.EEG_list)))
 
     def reReference(self, reference):
-        '''
+        """
         Rereference data to given channel
-        '''
+        """
 
         drop_index = set()
         for i in range(len(self.EEG_list)):
 
-            try: 
+            try:
                 index_reference = self.df_all.loc[i, 'channels'].index(reference)
-                self.EEG_list[i] = self.EEG_list[i] - self.EEG_list[i][index_reference,:]
+                self.EEG_list[i] = self.EEG_list[i] - self.EEG_list[i][index_reference, :]
 
             except ValueError:
                 # If CZ is not in the channels, drop it
@@ -91,7 +93,7 @@ class StressData:
         self.remove_trials(drop_index)
 
     def avg_channels_into_regions(self, mode=1):
-        '''
+        """
         Average over channels by six regions
 
         Parameter
@@ -104,10 +106,10 @@ class StressData:
         df_all : DataFrame
             contains channel information
 
-        mode : int 
+        mode : int
             1 : Average into 6 regions (frontal, left temporal, central, right temporal, parietal, occipital)
-            2 : Ignore channels on the central line and average into 9 regions 
-                (left frontal, right frontal, left temporal, left central, right central, right temporal, 
+            2 : Ignore channels on the central line and average into 9 regions
+                (left frontal, right frontal, left temporal, left central, right central, right temporal,
                  left parietal, right parietal, occipital)
 
         Return
@@ -116,14 +118,14 @@ class StressData:
             i : example
             j : region
             k : time sample
-        '''
+        """
 
-        assert mode in [1,2]
+        assert mode in [1, 2]
 
         if mode == 1:
-            regions = ['F','LT','C','RT','P','O']
+            regions = ['F', 'LT', 'C', 'RT', 'P', 'O']
         elif mode == 2:
-            regions = ['RF','LF','RT','LT','RC','LC','RP','LP','O']
+            regions = ['RF', 'LF', 'RT', 'LT', 'RC', 'LC', 'RP', 'LP', 'O']
 
         EEG_regions_list = []
 
@@ -139,119 +141,148 @@ class StressData:
                         indices_channels[0].append(j)
                     elif channel[0] == 'C':
                         indices_channels[2].append(j)
-                    elif channel[0] == 'P' or channel in ['T5','T6']:
+                    elif channel[0] == 'P' or channel in ['T5', 'T6']:
                         indices_channels[4].append(j)
                     elif channel[0] == 'O':
                         indices_channels[5].append(j)
-                    elif channel[0] == 'T' and int(channel[-1])%2 == 1:
+                    elif channel[0] == 'T' and int(channel[-1]) % 2 == 1:
                         indices_channels[1].append(j)
                     else:
                         indices_channels[3].append(j)
                 elif mode == 2 and channel[-1].lower() != 'z':
-                    if channel[0] == 'F' and int(channel[-1])%2 == 1:
+                    if channel[0] == 'F' and int(channel[-1]) % 2 == 1:
                         indices_channels[1].append(j)
-                    elif channel[0] == 'F' and int(channel[-1])%2 == 0:
+                    elif channel[0] == 'F' and int(channel[-1]) % 2 == 0:
                         indices_channels[0].append(j)
-                    elif channel[0] == 'C' and int(channel[-1])%2 == 1:
+                    elif channel[0] == 'C' and int(channel[-1]) % 2 == 1:
                         indices_channels[5].append(j)
-                    elif channel[0] == 'C' and int(channel[-1])%2 == 0:
+                    elif channel[0] == 'C' and int(channel[-1]) % 2 == 0:
                         indices_channels[4].append(j)
-                    elif (channel[0] == 'P' and int(channel[-1])%2 == 1) or channel=='T5':
+                    elif (channel[0] == 'P' and int(channel[-1]) % 2 == 1) or channel == 'T5':
                         indices_channels[7].append(j)
-                    elif (channel[0] == 'P' and int(channel[-1])%2 == 0) or channel=='T6':
+                    elif (channel[0] == 'P' and int(channel[-1]) % 2 == 0) or channel == 'T6':
                         indices_channels[6].append(j)
                     elif channel[0] == 'O':
                         indices_channels[8].append(j)
-                    elif channel[0] == 'T' and int(channel[-1])%2 == 1:
+                    elif channel[0] == 'T' and int(channel[-1]) % 2 == 1:
                         indices_channels[3].append(j)
                     else:
                         indices_channels[2].append(j)
-                              
 
             for i_region in range(len(regions)):
-                EEG_regions[i_region,:] = np.mean(self.EEG_list[i][indices_channels[i_region],:], axis=0)
+                EEG_regions[i_region, :] = np.mean(self.EEG_list[i][indices_channels[i_region], :], axis=0)
 
             EEG_regions_list.append(EEG_regions)
 
         # Replace channels with regions
         self.df_all['channels'] = [regions for _ in range(len(self.df_all))]
-        
+
         self.EEG_list = EEG_regions_list
-        
+
         # Remove trials with lack of regions
         indices = []
         for i in range(len(self.EEG_list)):
-            if np.sum(np.isnan(self.EEG_list[i]))>0:
+            if np.sum(np.isnan(self.EEG_list[i])) > 0:
                 indices.append(i)
-                
+
         if indices:
             print('Remove trials due to lack of regions')
             self.remove_trials(indices)
-        
+
+    def sliding_window_augmentation(self, window_size=10 * fs):
+        """
+        Augment data by sliding window without overlapping
+        """
+
+        new_EEG_list = []
+        new_label_list = []
+        num_window = min([data.shape[1] for data in
+                          self.EEG_list]) // window_size  # Get number of window based on data with shortest length
+
+        for i_data, EEG in enumerate(self.EEG_list):
+            for i in range(num_window):
+                new_EEG_list.append(EEG[:, i * window_size:(i + 1) * window_size])
+            new_label_list.extend([self.labels[i_data]]*num_window)
+
+        print('Sliding window augments number of data from {old_num} to {new_num} ({multiplier}x)'.format(
+            old_num=len(self.EEG_list), new_num=len(new_EEG_list), multiplier=num_window
+        ))
+        self.EEG_list = new_EEG_list
+        self.labels = np.array(new_label_list)
+        return num_window
+
     def get_coherence(self, low, high):
-        '''
+        """
         Add coherence of signal
-        
+
         Parameter
         ----------
         low: list of numbers
             Low frequency of frequency bands
         high: list of numbers
             High frequency of freqeuncy bands
-            
+
         Return
         ----------
         coherence: numpy 2d array
-            Magnitude squared coherence of of region combinations 
-        
-        '''
-        assert len(low)==len(high) and all([low[i]<=high[i] for i in range(len(low))])
-        assert all(len(self.EEG_list[i])==len(self.EEG_list[0]) for i in range(len(self.EEG_list)))
-        assert all(self.df_all.loc[i, 'channels']==self.df_all.loc[0, 'channels'] for i in range(len(self.df_all)))
-        
+            Magnitude squared coherence of of region combinations
+
+        """
+        assert len(low) == len(high) and all([low[i] <= high[i] for i in range(len(low))])
+        assert all(len(self.EEG_list[i]) == len(self.EEG_list[0]) for i in range(len(self.EEG_list)))
+        assert all(self.df_all.loc[i, 'channels'] == self.df_all.loc[0, 'channels'] for i in range(len(self.df_all)))
+
         num_channels = len(self.EEG_list[0])
         num_samples = len(self.EEG_list)
         num_bands = len(low)
-        coherence = np.zeros( (num_samples, int(num_channels*(num_channels-1)/2), num_bands) )
-        win = 5*fs
-        
+        coherence = np.zeros((num_samples, int(num_channels * (num_channels - 1) / 2), num_bands))
+        win = 5 * fs
+
         # Calculate coherence for each signal
         for i_sample in range(len(self.EEG_list)):
-            
+
             for i_comb, (r1, r2) in enumerate(itertools.combinations(range(num_channels), r=2)):
-                freqs, C = scipy.signal.coherence(self.EEG_list[i_sample][r1,:], 
-                                                  self.EEG_list[i_sample][r2,:], fs=fs, nperseg=win, noverlap=win//2)
-                
+                freqs, C = scipy.signal.coherence(self.EEG_list[i_sample][r1, :],
+                                                  self.EEG_list[i_sample][r2, :], fs=fs, nperseg=win, noverlap=win // 2)
+
                 # Find intersecting values in frequency vector
-                idx = np.logical_and(freqs[:,np.newaxis] >= low, freqs[:,np.newaxis] <= high)
-                idx = idx.T   # (65,3)->(3,65)
-                
+                idx = np.logical_and(freqs[:, np.newaxis] >= low, freqs[:, np.newaxis] <= high)
+                idx = idx.T  # (65,3)->(3,65)
+
                 for i_band in range(idx.shape[0]):
-                    coherence[i_sample, i_comb, i_band] = np.mean(C[idx[i_band,:]])
-                    
-        return coherence.reshape((len(coherence),-1))
-                
+                    coherence[i_sample, i_comb, i_band] = np.mean(C[idx[i_band, :]])
+
+        return coherence.reshape((len(coherence), -1))
+
     def get_covariance(self):
-        '''
+        """
         Calculate covariance matrix acroos channels for each trial
-        
+
         Return
         -----------
         covariance: 3d array
             i: num_trials
             j, k: num_channels
-        '''
-        
-        assert all(len(self.EEG_list[i])==len(self.EEG_list[0]) for i in range(len(self.EEG_list)))
-        
-        return np.array([Covariances('oas').transform(EEG[np.newaxis,:,:])[0] for EEG in self.EEG_list])
+        """
+
+        assert all(len(self.EEG_list[i]) == len(self.EEG_list[0]) for i in range(len(self.EEG_list)))
+
+        return np.array([Covariances('oas').transform(EEG[np.newaxis, :, :])[0] for EEG in self.EEG_list])
+
 
 def select_features(X_train, X_test, Y_train):
-    
-    assert isinstance(X_train, np.ndarray) and X_train.ndim==2
-    
+    assert isinstance(X_train, np.ndarray) and X_train.ndim == 2
+
     sel_ = SelectFromModel(LogisticRegression(C=1, penalty='l1', solver='liblinear'))
     X_train = sel_.fit_transform(X_train, Y_train)
     X_test = sel_.transform(X_test)
-    
+
     return X_train, X_test, sel_
+
+
+if __name__ == '__main__':
+    X, Y, df_all = dataloader.read_data(label_format=2, data_folder='resting_ASR_ref')
+    data = StressData(X, Y, df_all)
+    num_window = data.sliding_window_augmentation()
+    print('len(data.EEG_list): ', len(data.EEG_list))
+    print('len(data.labels): ', len(data.labels))
